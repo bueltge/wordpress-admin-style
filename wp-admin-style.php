9b0017cbf360620a8e6c8526743b118db90be47f
Frank Bültge <frank@bueltge.de>
2014-04-29 12:27:17 +0200

    release new version, fixed #16

--- wp-admin-style.php
+++ wp-admin-style.php
@@ -6,10 +6,10 @@
  * Domain Path:   /languages
  * Description:   Shows the WordPress admin styles on one page to help you to develop WordPress compliant
  * Author:        Frank Bültge
- * Version:       1.0.1
+ * Version:       1.0.2
  * Licence:       GPLv2
  * Author URI:    http://bueltge.de
- * Last Change:   02/23/2014
+ * Last Change:   02/29/2014
  */
 
 /**
