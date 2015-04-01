36cf122746c1ede4c3ecb968dfe7e1620279a840
lriaudel <lriaudel@gmail.com>
2015-04-01 13:59:34 +0200

    Divided "2 columns" in two files Add "2 columns: left and right cols" Add lorem ispum in layout Add toggle html tag (but I don't find js script for active it) Add class "hndle" for <h3> in layout for subline

---
+++ patterns/two-column-layout-2.php
@@ -0,0 +1,37 @@
+
+<h3><?php _e( '2 Columns Layout : 35% +65%', 'wp_admin_style' ); ?></h3>
+
+<p>The cols dimensions is customize by css, exemple:<br/>
+.customposttype #col-left{ width: 25%;}<br/>
+.customposttype #col-right{ width: 75%;}
+</p>
+
+<div class="wrap">
+
+    <div id="col-container">
+
+        <div id="col-right">
+            
+            <div class="col-wrap">
+                Col right content
+                <div class="inside">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</div>
+
+            </div> <!-- /col-wrap -->
+            
+        </div><!-- /col-right -->
+
+        <div id="col-left">
+            
+            <div class="col-wrap">
+                Col left content
+                <div class="inside">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </div>
+            </div> <!-- /col-wrap -->
+            
+        </div><!-- /col-left -->
+
+    </div><!-- /col-container -->
+    
+</div> <!-- .wrap -->
