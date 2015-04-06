ced142498c3b8b8b5e9df763e81e8e7031b31264
bueltge <frank@bueltge.de>
2015-04-06 17:17:27 +0200

    Add default admin color scheme which hex code

---
+++ patterns/color-scheme-blue.php
@@ -0,0 +1,22 @@
+<h3><?php esc_attr_e( 'Color Scheme: Blues', 'wp_admin_style' ); ?></h3>
+<table style="width: 80%;text-align: center;border: 0 none">
+	<tbody>
+	<tr>
+		<td style="border: 0 none">smaller WordPress 4.2</td>
+		<td style="border: 0 none">since WordPress 4.2</td>
+	</tr>
+	<tr>
+		<td style="background: #0074a2;color: #fff;border: 2px solid #fff;padding: 2em 0">#0074a2</td>
+		<td style="background: #0073aa;color: #fff;border: 2px solid #fff;padding: 2em 0">#0073aa</td>
+	</tr>
+	<tr>
+		<td style="background: #2ea2cc;color: #fff;border: 2px solid #fff;padding: 2em 0">#2ea2cc</td>
+		<td style="background: #00a0d2;color: #fff;border: 2px solid #fff;padding: 2em 0">#00a0d2</td>
+	</tr>
+	<tr>
+		<td style="background: #45bbe6;color: #fff;border: 2px solid #fff;padding: 2em 0">#45bbe6</td>
+		<td style="background: #00b9eb;color: #fff;border: 2px solid #fff;padding: 2em 0">#00b9eb</td>
+	</tr>
+	</tbody>
+</table>
+<p><a href="https://make.wordpress.org/design/2015/04/05/default-admin-color-scheme-update/">Background in WordPress Make Blog</a></p>
