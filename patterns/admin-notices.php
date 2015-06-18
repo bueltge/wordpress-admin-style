84b4afff0f358dab1477eceb5f826041b8db9a50
bueltge <frank@bueltge.de>
2015-06-18 12:32:42 +0200

    Improve the admin notice for the new possibilties, since wp 4.2; fixed #27

--- patterns/admin-notices.php
+++ patterns/admin-notices.php
@@ -1,5 +1,6 @@
 <h3><?php esc_attr_e( 'Admin Notices', 'wp_admin_style' ); ?></h3>
 <p><?php esc_attr_e( 'define the style via param (same as the classes) on function add_settings_error() or use the class inside a div', 'wp_admin_style' ); ?></p>
+<p><?php printf( __( 'Since WordPress version 4.2 give it much more classes and paths. See more information and background in this <a href="%s">post</a>.', 'wp_admin_style' ), 'https://make.wordpress.org/core/2015/04/23/spinners-and-dismissible-admin-notices-in-4-2/' ); ?></p>
 
 <p><strong><?php esc_attr_e( 'HINT: The Admin Notices class was moved on default via wp-admin/js/common.js to the top after the first h2. See on top.', 'wp_admin_style' ) ?></strong></p>
 
@@ -7,3 +8,4 @@
 <div class="error"><?php esc_attr_e( 'class .error without paragraph', 'wp_admin_style' ); ?></div>
 <div class="settings-error"><?php esc_attr_e( 'class .settings-error without paragraph', 'wp_admin_style' ); ?></div>
 <div class="error form-invalid"><?php esc_attr_e( 'class .error and .form-invalid without paragraph', 'wp_admin_style' ); ?></div>
+<div class="notice"><p><?php esc_attr_e( 'class .notice with paragraph', 'wp_admin_style' ); ?></p></div>
--- patterns/spinners.php
+++ patterns/spinners.php
@@ -1,4 +1,4 @@
 <h3><?php esc_html_e( 'Spinners', 'wp_admin_style' ); ?></h3>
 <p><?php printf( __( 'The Spinner element is new since WordPress version 4.2. Much more information and background is in this <a href="%s">post</a>.', 'wp_admin_style' ), 'https://make.wordpress.org/core/2015/04/23/spinners-and-dismissible-admin-notices-in-4-2/' ); ?></p>
 
-<div class="spinner" style="visibility:inherit;float:none;width:auto;height:auto;padding:10px 0 10px 50px;background-position:20px 0;"><?php esc_attr_e( 'class .spinner in a div-tag with custom style for demonstration.', 'wp_admin_style' ); ?></div>
+<div class="spinner is-active" style="float:none;width:auto;height:auto;padding:10px 0 10px 50px;background-position:20px 0;"><?php esc_attr_e( 'class .spinner in a div-tag with custom style for demonstration.', 'wp_admin_style' ); ?></div>
