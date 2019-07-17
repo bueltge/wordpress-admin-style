9e8e04a29fd5f1b7783791ac3cf7ee8256076797
Stefan Pejcic <pejcha1994@gmail.com>
2019-07-17 16:56:44 +0200

    updated colors

--- patterns/color-scheme-blue.php
+++ patterns/color-scheme-blue.php
@@ -1,23 +1,16 @@
-<h2><?php esc_attr_e( 'Color Scheme: Blue', 'WpAdminStyle' ); ?></h2>
+<h2><?php esc_attr_e( 'Colors: Blue', 'WpAdminStyle' ); ?></h2>
+<p><?php printf( __( 'See <a href="%s" target="_blank">the official color guide on make.w.org</a> for further details.',
+                     'WpAdminStyle' ), esc_url( 'https://make.wordpress.org/design/handbook/design-guide/foundations/colors/' ) ); ?></p>
+
 <table style="width: 80%;text-align: center;border: 0 none">
 	<tbody>
 	<tr>
-		<td style="border: 0 none"><?php _e( 'prior WordPress 4.2', 'WpAdminStyle' ); ?></td>
-		<td style="border: 0 none"><?php _e( 'since WordPress 4.2', 'WpAdminStyle' ); ?></td>
-	</tr>
-	<tr>
-		<td style="background: #0074a2;color: #fff;border: 2px solid #fff;padding: 2em 0">#0074a2</td>
-		<td style="background: #0073aa;color: #fff;border: 2px solid #fff;padding: 2em 0">#0073aa</td>
-	</tr>
-	<tr>
-		<td style="background: #2ea2cc;color: #fff;border: 2px solid #fff;padding: 2em 0">#2ea2cc</td>
-		<td style="background: #00a0d2;color: #fff;border: 2px solid #fff;padding: 2em 0">#00a0d2</td>
-	</tr>
-	<tr>
-		<td style="background: #45bbe6;color: #fff;border: 2px solid #fff;padding: 2em 0">#45bbe6</td>
-		<td style="background: #00b9eb;color: #fff;border: 2px solid #fff;padding: 2em 0">#00b9eb</td>
+		<td style="width:40%; background: #0073AA;color: #fff;border: 2px solid #fff;padding: 2em 0"><b>WORDPRESS BLUE</b><br>
+RGB(0,115,170)<br>
+#0073AA</td>
+		<td style="width:40%; background: #00A0D2;color: #fff;border: 2px solid #fff;padding: 2em 0"><b>MEDIUM BLUE</b><br>
+RGB(0,160,210)<br>
+#00A0D2</td>
 	</tr>
 	</tbody>
 </table>
-<p><?php printf( __( 'See <a href="%s" target="_blank">this post on make.w.org</a> for further details.',
-                     'WpAdminStyle' ), esc_url( 'https://make.wordpress.org/design/2015/04/05/default-admin-color-scheme-update/' ) ); ?></p>
