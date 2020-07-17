0cea96a4a8add51c4e59b23441b6f5ed358415f5
Bueltge <frank@bueltge.de>
2020-07-17 12:11:25 +0200

    Add card, #60

---
+++ patterns/cards.php
@@ -0,0 +1,8 @@
+<h2><?php esc_attr_e( 'Card', 'WpAdminStyle' ); ?></h2>
+<div class="card">
+	<h2 class="title"><?php esc_attr_e( 'Main Content Title', 'WpAdminStyle' ); ?></h2>
+	<p><?php esc_attr_e(
+			'WordPress started in 2003 with a single bit of code to enhance the typography of everyday writing and with fewer users than you can count on your fingers and toes. Since then it has grown to be the largest self-hosted blogging tool in the world, used on millions of sites and seen by tens of millions of people every day.',
+			'WpAdminStyle'
+		); ?></p>
+</div>
