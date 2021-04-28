f064afa24894baee49a8e4e78858734e10b1f035
Frank Bueltge <frank@bueltge.de>
2021-04-28 11:05:18 +0200

    Update picker for jquery 3.8*

--- js/dashicons-picker.js
+++ js/dashicons-picker.js
@@ -5,7 +5,7 @@
  */
 
 ( function ( $ ) {
-
+	'use strict';
 	/**
 	 *
 	 * @returns {void}
@@ -322,50 +322,60 @@
 			'fullscreen-alt',
 			'fullscreen-exit-alt',
 			'',
+			'',
 			''
 		];
 
 		return this.each( function () {
 
-			var button = $( this );
+			var button = $( this ),
+				offsetTop,
+				offsetLeft;
 
-			button.on( 'click.dashiconsPicker', function () {
+			button.on( 'click.dashiconsPicker', function ( e ) {
+				offsetTop = $( e.currentTarget ).offset().top;
+				offsetLeft = $( e.currentTarget ).offset().left;
 				createPopup( button );
 			} );
 
 			function createPopup( button ) {
 
 				var target = $( button.data( 'target' ) ),
-					popup  = $( '<div class="dashicon-picker-container"> \
-						<div class="dashicon-picker-control" /> \
-						<ul class="dashicon-picker-list" /> \
-					</div>' )
-						.css( {
-							'top':  button.offset().top,
-							'left': button.offset().left
-						} ),
+					preview = $( button.data( 'preview' ) ),
+					popup  = $( '<div class="dashicon-picker-container">' +
+						'<div class="dashicon-picker-control"></div>' +
+						'<ul class="dashicon-picker-list"></ul>' +
+					'</div>' ).css( {
+						'top':  offsetTop,
+						'left': offsetLeft
+					} ),
 					list = popup.find( '.dashicon-picker-list' );
 
 				for ( var i in icons ) {
-					list.append( '<li data-icon="' + icons[i] + '"><a href="#" title="' + icons[i] + '"><span class="dashicons dashicons-' + icons[i] + '"></span></a></li>' );
-				};
+					if ( icons.hasOwnProperty(i) ) {
+						list.append('<li data-icon="' + icons[i] + '"><a href="#" title="' + icons[i] + '"><span class="dashicons dashicons-' + icons[i] + '"></span></a></li>');
+					}
+				}
 
-				$( 'a', list ).click( function ( e ) {
+				$( 'a', list ).on( 'click', function ( e ) {
 					e.preventDefault();
 					var title = $( this ).attr( 'title' );
 					target.val( 'dashicons-' + title );
+					preview
+						.prop('class', 'dashicons')
+						.addClass( 'dashicons-' + title );
 					removePopup();
 				} );
 
 				var control = popup.find( '.dashicon-picker-control' );
 
-				control.html( '<a data-direction="back" href="#"> \
-					<span class="dashicons dashicons-arrow-left-alt2"></span></a> \
-					<input type="text" class="" placeholder="Search" /> \
-					<a data-direction="forward" href="#"><span class="dashicons dashicons-arrow-right-alt2"></span></a>'
+				control.html( '<a data-direction="back" href="#">' +
+					'<span class="dashicons dashicons-arrow-left-alt2"></span></a>' +
+					'<input type="text" class="" placeholder="Search" />' +
+					'<a data-direction="forward" href="#"><span class="dashicons dashicons-arrow-right-alt2"></span></a>'
 				);
 
-				$( 'a', control ).click( function ( e ) {
+				$( 'a', control ).on( 'click', function ( e ) {
 					e.preventDefault();
 					if ( $( this ).data( 'direction' ) === 'back' ) {
 						$( 'li:gt(' + ( icons.length - 26 ) + ')', list ).prependTo( list );
@@ -391,7 +401,7 @@
 					}
 				} );
 
-				$( document ).bind( 'mouseup.dashicons-picker', function ( e ) {
+				$( document ).on( 'mouseup.dashicons-picker', function ( e ) {
 					if ( ! popup.is( e.target ) && popup.has( e.target ).length === 0 ) {
 						removePopup();
 					}
@@ -400,7 +410,7 @@
 
 			function removePopup() {
 				$( '.dashicon-picker-container' ).remove();
-				$( document ).unbind( '.dashicons-picker' );
+				$( document ).off( '.dashicons-picker' );
 			}
 		} );
 	};
